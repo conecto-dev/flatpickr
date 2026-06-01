conecto-dev/flatpickr

commit 366b855ad1d8924a729c817c47b192d604b3ac2b

fix: split month event listener, allow native events on button/select

src/index.ts
@@ -458,8 +458,9 @@ function FlatpickrInstance(
     }
 
     if (self.daysContainer !== undefined) {
-      bind(self.monthNav, "click", onMonthNavClick);
-      bind(self.monthNav, ["keyup", "increment"], onYearInput);
+      bind(self.prevMonthNav, "click", onMonthNavClick);
+      bind(self.nextMonthNav, "click", onMonthNavClick);
+      bind(self.monthsDropdownContainer, ["keyup", "increment"], onYearInput);
       bind(self.daysContainer, "click", selectDate);
     }
 
@@ -1681,17 +1682,6 @@ function FlatpickrInstance(
   }
 
   function onKeyDown(e: KeyboardEvent) {
-    // e.key                      e.keyCode
-    // "Backspace"                        8
-    // "Tab"                              9
-    // "Enter"                           13
-    // "Escape"     (IE "Esc")           27
-    // "ArrowLeft"  (IE "Left")          37
-    // "ArrowUp"    (IE "Up")            38
-    // "ArrowRight" (IE "Right")         39
-    // "ArrowDown"  (IE "Down")          40
-    // "Delete"     (IE "Del")           46
-
     const eventTarget = getEventTarget(e);
     const isInput = self.config.wrap
       ? element.contains(eventTarget as HTMLElement)
@@ -1754,14 +1744,7 @@ function FlatpickrInstance(
             e.preventDefault();
             updateTime();
             focusAndClose();
-          } else if (
-            eventTarget === self.currentMonthElement &&
-            self.monthsDropdownContainer
-          ) {
-            e.preventDefault();
-            self.monthsDropdownContainer.focus();
-          } else selectDate(e);
-
+          }
           break;
 
         case "Escape": // escape
@@ -3021,10 +3004,6 @@ function FlatpickrInstance(
 
     if (isPrevMonth || isNextMonth) {
       changeMonth(isPrevMonth ? -1 : 1);
-    } else if (
-      self.yearElements.indexOf(eventTarget as HTMLInputElement) >= 0
-    ) {
-      (eventTarget as HTMLInputElement).select();
     } else if ((eventTarget as Element).classList.contains("arrowUp")) {
       self.changeYear(self.currentYear + 1);
     } else if ((eventTarget as Element).classList.contains("arrowDown")) {

src/types/instance.ts
@@ -27,7 +27,6 @@ export interface Elements {
 
   // month nav
   monthNav: HTMLDivElement;
-
   monthsDropdownContainer: HTMLSelectElement;
 
   yearElements: HTMLInputElement[];
@@ -40,8 +39,8 @@ export interface Elements {
   // month nav arrows
   _hidePrevMonthArrow: boolean;
   _hideNextMonthArrow: boolean;
-  prevMonthNav: HTMLElement;
-  nextMonthNav: HTMLElement;
+  prevMonthNav: HTMLButtonElement;
+  nextMonthNav: HTMLButtonElement;
 
   timeContainer?: HTMLDivElement;
   hourElement?: HTMLInputElement;

commit 218cd73b138177a7f08db452bfa67b7f1e0528b2

fix: keyboard and range selection bugs
  - Restore Enter key date selection (broken since 366b855a)
  - Enter on prev/next month nav now changes month
  - Tab respects trapFocus: false (no longer traps in inline picker)
  - rangePlugin: reset _prevDates on clear so click/Space can start
    a new range without forcing the previous end date

src/index.ts
@@ -460,7 +460,8 @@ function FlatpickrInstance(
     if (self.daysContainer !== undefined) {
       bind(self.prevMonthNav, "click", onMonthNavClick);
       bind(self.nextMonthNav, "click", onMonthNavClick);
-      bind(self.monthsDropdownContainer, ["keyup", "increment"], onYearInput);
+      if (self.monthsDropdownContainer)
+        bind(self.monthsDropdownContainer, ["keyup", "increment"], onYearInput);
       bind(self.daysContainer, "click", selectDate);
     }
 
@@ -1749,6 +1750,26 @@ function FlatpickrInstance(
             e.preventDefault();
             updateTime();
             focusAndClose();
+          } else if (
+            eventTarget === self.currentMonthElement &&
+            self.monthsDropdownContainer
+          ) {
+            e.preventDefault();
+            self.monthsDropdownContainer.focus();
+          } else if (
+            self.prevMonthNav &&
+            self.prevMonthNav.contains(eventTarget as Node)
+          ) {
+            e.preventDefault();
+            changeMonth(-1);
+          } else if (
+            self.nextMonthNav &&
+            self.nextMonthNav.contains(eventTarget as Node)
+          ) {
+            e.preventDefault();
+            changeMonth(1);
+          } else {
+            selectDate(e);
           }
           break;
 
@@ -1816,8 +1837,6 @@ function FlatpickrInstance(
 
         case "Tab":
           if (isCalendarElem(eventTarget as HTMLElement) || isTimeObj) {
-            e.preventDefault();
-
             const tabbableElements: HTMLElement[] = [];
 
             if (self.prevMonthNav && !self._hidePrevMonthArrow) {
@@ -1887,9 +1906,10 @@ function FlatpickrInstance(
             if (currentIndex !== -1) {
               const direction = e.shiftKey ? -1 : 1;
               const nextIndex = currentIndex + direction;
-              let targetElement: HTMLElement | null = null;
 
               if (self.config.trapFocus) {
+                e.preventDefault();
+                let targetElement: HTMLElement;
                 if (nextIndex < 0) {
                   targetElement = tabbableElements[tabbableElements.length - 1];
                 } else if (nextIndex >= tabbableElements.length) {
@@ -1897,16 +1917,13 @@ function FlatpickrInstance(
                 } else {
                   targetElement = tabbableElements[nextIndex];
                 }
-              } else {
-                if (nextIndex < 0 || nextIndex >= tabbableElements.length) {
-                  self._input.focus();
-                } else {
-                  targetElement = tabbableElements[nextIndex];
-                }
-              }
-
-              if (targetElement) {
                 targetElement.focus();
+              } else {
+                if (nextIndex >= 0 && nextIndex < tabbableElements.length) {
+                  e.preventDefault();
+                  tabbableElements[nextIndex].focus();
+                }
+                // else: let browser handle Tab naturally out of calendar
               }
             }
           }

src/plugins/rangePlugin.ts
@@ -16,7 +16,7 @@ function rangePlugin(config: Config = {}): Plugin {
     let dateFormat = "",
       secondInput: HTMLInputElement,
       _secondInputFocused: boolean,
-      _prevDates: Date[];
+      _prevDates: Date[] | undefined;
 
     const createSecondInput = () => {
       if (config.input) {
@@ -135,6 +135,7 @@ function rangePlugin(config: Config = {}): Plugin {
           fp.latestSelectedDateObj = fp.selectedDates[0];
           fp._setHoursFromDate(fp.selectedDates[0]);
           _secondInputFocused = false;
+          _prevDates = undefined;
           fp.jumpToDate(fp.selectedDates[0]);
         });
 
@@ -164,11 +165,11 @@ function rangePlugin(config: Config = {}): Plugin {
 
       onChange() {
         if (!fp.selectedDates.length) {
+          _prevDates = undefined;
           setTimeout(() => {
             if (fp.selectedDates.length) return;
 
             secondInput.value = "";
-            _prevDates = [];
           }, 10);
         }
 